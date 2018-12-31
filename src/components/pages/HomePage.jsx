3927e1e93fe854a8093acb8a32429cd555e751c3
soul <soul@souldeiMac.local>
2018-12-31 11:27:39 +0800

    update homePage

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
-import { View, Link } from 'framework7-react';
+import { View, Link, Page, Button } from 'framework7-react';
 
 export default class HomePage extends React.Component {
   render() {
     return (
-      <View>
-        <Link href="/counter/">counter</Link>
-        <Link href="/hoc/">HOC</Link>
-        <Link href="/stop-watch/">stopwatch</Link>
-        <Link href="/redux/counter">Redux Counter</Link>
-      </View>
+      <Page>
+        <Button>
+          <Link href="/counter/">counter</Link>
+        </Button>
+        <Button>
+          <Link href="/hoc/">HOC</Link>
+        </Button>
+        <Button>
+          <Link href="/stop-watch/">stopwatch</Link>
+        </Button>
+        <Button>
+          <Link href="/redux/counter">Redux Counter</Link>
+        </Button>
+      </Page>
     )
   }
 }
